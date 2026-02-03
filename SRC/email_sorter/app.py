b124ddee849218d023364c47c6eb8ebaf53e71bb
jun7503 <junehong.kwon@sonceboz.com>
2026-02-03 11:03:30 +0900

    Update app.py

--- SRC/email_sorter/app.py
+++ SRC/email_sorter/app.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QApplication
-from .gui import EmailSorterWindow
-from .config_loader import load_config
+from email_sorter.gui import EmailSorterWindow
+from email_sorter.config_loader import load_config
 
 def main():
     cfg = load_config()
