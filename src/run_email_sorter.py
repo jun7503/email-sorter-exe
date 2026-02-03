7a2fe99f0e7bfff85ef31683b8311f0d41bfecee
jun7503 <junehong.kwon@sonceboz.com>
2026-02-03 14:12:02 +0900

    Update run_email_sorter.py

--- src/run_email_sorter.py
+++ src/run_email_sorter.py
@@ -1,5 +1,10 @@
-# src/run_email_sorter.py
+import sys
 from email_sorter.app import main
 
 if __name__ == "__main__":
     main()
+
+    # Keep window open when running the EXE
+    if getattr(sys, 'frozen', False):
+        print("\nDEBUG: Finished running EmailSorter.exe")
+        input("Press Enter to exit...")
